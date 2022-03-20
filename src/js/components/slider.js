96e9ecf78cb4b040c50203d83781de03fa1c0204
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-03-21 02:32:07 +0300

    настройка слайдера events

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -53,55 +53,46 @@ const sliderGallery = new Swiper(".gallery__slider", {
   }
 })
 
-const sliderEvents = new Swiper(".events__slider", {
+const eventsSliders = document.querySelector(".events__slider")
+
+const sliderEvents = new Swiper(eventsSliders, {
+  slidesPerView: 3,
   navigation: {
-    nextEl: '.slider-control__arrow--next',
-    prevEl: '.slider-control__arrow--prev',
+    nextEl: '.events__next',
+    prevEl: '.events__prev',
   },
-  pagination: {
-    el: '.slider-control__fraction',
-    type: 'fraction'
-  },
-  updateOnWindowResize: true,
-  a11y: {
-    enabled: true,
-    prevSlideMessage: 'Предыдущий слайд',
-    nextSlideMessage: 'Следующий слайд',
-    firstSlideMessage: 'Это первый слайд',
-    lastSlideMessage: 'Это последний слайд',
-    paginationBulletMessage: 'перейти к слайду {{index}}',
-    notificationClass: 'swiper-notification',
-  },
-  breakpoints: {
-    500: {
-      slidesPerColumnFill: 'row',
-      // slidesPerGroup: 1,
-      slidesPerView: 1,
-      slidesPerColumn: 0,
-      updateOnWindowResize: true,
-    },
-    720: {
-      slidesPerColumnFill: 'row',
-      slidesPerGroup: 2,
-      slidesPerView: 2,
-      slidesPerColumn: 1,
-      updateOnWindowResize: true,
-      spaceBetween: 34,
-    },
-    1024: {
-      slidesPerColumnFill: 'row',
-      // slidesPerGroup: 2,
-      slidesPerView: 2,
-      slidesPerColumn: 1,
-      updateOnWindowResize: true,
-      spaceBetween: 34,
-    },
-    1330: {
-      spaceBetween: 50,
-      slidesPerColumnFill: 'row',
-      updateOnWindowResize: true,
-      slidesPerView: 3,
-      slidesPerColumn: 1,
-    }
-  }
+  spaceBetween: 50,
+  slidesPerColumn: 1,
+  // breakpoints: {
+  //   500: {
+  //     slidesPerColumnFill: 'row',
+  //     // slidesPerGroup: 1,
+  //     slidesPerView: 1,
+  //     slidesPerColumn: 0,
+  //     updateOnWindowResize: true,
+  //   },
+  //   720: {
+  //     slidesPerColumnFill: 'row',
+  //     slidesPerGroup: 2,
+  //     slidesPerView: 2,
+  //     slidesPerColumn: 1,
+  //     updateOnWindowResize: true,
+  //     spaceBetween: 34,
+  //   },
+  //   1024: {
+  //     slidesPerColumnFill: 'row',
+  //     // slidesPerGroup: 2,
+  //     slidesPerView: 2,
+  //     slidesPerColumn: 1,
+  //     updateOnWindowResize: true,
+  //     spaceBetween: 34,
+  //   },
+  //   1330: {
+  //     spaceBetween: 50,
+  //     slidesPerColumnFill: 'row',
+  //     updateOnWindowResize: true,
+  //     slidesPerView: 3,
+  //     slidesPerColumn: 1,
+  //   }
+  // }
 })
