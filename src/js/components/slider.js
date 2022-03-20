c2590202a6a03602d7313e391a363812bb98f40e
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-03-21 00:53:48 +0300

    верстка events

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -52,3 +52,56 @@ const sliderGallery = new Swiper(".gallery__slider", {
     }
   }
 })
+
+const sliderEvents = new Swiper(".events__slider", {
+  navigation: {
+    nextEl: '.slider-control__arrow--next',
+    prevEl: '.slider-control__arrow--prev',
+  },
+  pagination: {
+    el: '.slider-control__fraction',
+    type: 'fraction'
+  },
+  updateOnWindowResize: true,
+  a11y: {
+    enabled: true,
+    prevSlideMessage: 'Предыдущий слайд',
+    nextSlideMessage: 'Следующий слайд',
+    firstSlideMessage: 'Это первый слайд',
+    lastSlideMessage: 'Это последний слайд',
+    paginationBulletMessage: 'перейти к слайду {{index}}',
+    notificationClass: 'swiper-notification',
+  },
+  breakpoints: {
+    500: {
+      slidesPerColumnFill: 'row',
+      // slidesPerGroup: 1,
+      slidesPerView: 1,
+      slidesPerColumn: 0,
+      updateOnWindowResize: true,
+    },
+    720: {
+      slidesPerColumnFill: 'row',
+      slidesPerGroup: 2,
+      slidesPerView: 2,
+      slidesPerColumn: 1,
+      updateOnWindowResize: true,
+      spaceBetween: 34,
+    },
+    1024: {
+      slidesPerColumnFill: 'row',
+      // slidesPerGroup: 2,
+      slidesPerView: 2,
+      slidesPerColumn: 1,
+      updateOnWindowResize: true,
+      spaceBetween: 34,
+    },
+    1330: {
+      spaceBetween: 50,
+      slidesPerColumnFill: 'row',
+      updateOnWindowResize: true,
+      slidesPerView: 3,
+      slidesPerColumn: 1,
+    }
+  }
+})
